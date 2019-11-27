ccb03da9d9884cb605e7dcf918ee3fa89ad44807
Samih Soylu <7690781+SamihSoylu@users.noreply.github.com>
2019-11-27 10:59:17 +0100

    Code improvements to json file
    Now exposing full file path in exception

--- private/lib/Utilities/JSONFile.php
+++ private/lib/Utilities/JSONFile.php
@@ -18,17 +18,14 @@ class JSONFile
     public static function read(string $filePath): array
     {
         if (!file_exists($filePath)) {
-            throw new FileNotFoundException("File '".$filePath."' could not be found, please make sure the file exists.");
+            throw new FileNotFoundException("File '{$filePath}' could not be found, please make sure the file exists.");
         }
 
         $fileData = file_get_contents($filePath);
-        $jsonData  = JSON_decode($fileData, true);
+        $jsonData = JSON_decode($fileData, true);
 
         if ($jsonData === null) {
-            $fileName = explode('/', $filePath);
-            $fileName = end($fileName);
-
-            throw new InvalidJsonFileException("Failed to decode: '{$fileName}' json file.");
+            throw new InvalidJsonFileException("Failed to decode: '{$filePath}' json file.");
         }
 
         return $jsonData;
