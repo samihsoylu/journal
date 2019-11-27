e22e928945af90b6810e22a6d1c570432143ddb7
Samih Soylu <7690781+SamihSoylu@users.noreply.github.com>
2019-11-27 14:43:16 +0100

    Implemented write method and applied corrections to read method
    We now take in to consideration that a file might not be readable in the read() method.

--- private/lib/Utilities/JSONFile.php
+++ private/lib/Utilities/JSONFile.php
@@ -4,6 +4,7 @@ namespace App\Utilities;
 
 use App\Exceptions\FileNotFoundException;
 use App\Exceptions\InvalidJsonFileException;
+use \RuntimeException;
 
 /**
  * JSONFile class that deals with reading and writing json files.
@@ -17,17 +18,35 @@ class JSONFile
      */
     public static function read(string $filePath): array
     {
-        if (!file_exists($filePath)) {
-            throw new FileNotFoundException("File '{$filePath}' could not be found, please make sure the file exists.");
+        if (!file_exists($filePath) || !is_readable($filePath)) {
+            throw new FileNotFoundException("File '{$filePath}' could not be opened, make sure the file exists.");
         }
 
         $fileData = file_get_contents($filePath);
-        $jsonData = JSON_decode($fileData, true);
+        $jsonData = json_decode($fileData, true);
 
         if ($jsonData === null) {
             throw new InvalidJsonFileException("Failed to decode: '{$filePath}' json file.");
         }
 
         return $jsonData;
+    }
+    
+    /**
+     * @throws RuntimeException
+     */
+    public static function write(string $filePath, array $data): bool
+    {
+        $jsonData = json_encode($data, JSON_PRETTY_PRINT);
+        if ($jsonData === false) {
+            throw new RuntimeException('Could not json encode data: ' . print_r($data, true));
+        }
+
+        $write = file_put_contents($filePath, $jsonData);
+        if ($write === false) {
+            throw new RuntimeException("Unable to write to file {$filePath}");
+        }
+
+        return true;
     }
 }
