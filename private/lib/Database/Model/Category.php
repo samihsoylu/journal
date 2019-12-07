606834beb9e8fa8440d94c36c688e40ef0a47643
Samih Soylu <mail@samihsoylu.nl>
2019-12-07 13:15:48 +0100

    Created category and note models

---
+++ private/lib/Database/Model/Category.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Database\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="categories")
+ */
+class Category extends AbstractModel
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $categoryName;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $createdTimestamp;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $lastUpdatedTimestamp;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getCategoryName(): string
+    {
+        return $this->categoryName;
+    }
+
+    public function setCategoryName(string $categoryName): self
+    {
+        $this->categoryName = $categoryName;
+        return $this;
+    }
+
+    public function getCreatedTimestamp(): int
+    {
+        return $this->createdTimestamp;
+    }
+
+    public function setCreatedTimestamp(int $createdTimestamp): self
+    {
+        $this->createdTimestamp = $createdTimestamp;
+        return $this;
+    }
+
+    public function getLastUpdatedTimestamp(): int
+    {
+        return $this->lastUpdatedTimestamp;
+    }
+
+    public function setLastUpdatedTimestamp(int $lastUpdatedTimestamp): self
+    {
+        $this->lastUpdatedTimestamp = $lastUpdatedTimestamp;
+        return $this;
+    }
+}
---
+++ private/lib/Database/Model/Note.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Database\Model;
+
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="notes")
+ */
+class Note extends AbstractModel
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ManyToOne(targetEntity="Category")
+     * @JoinColumn(name="categoryId", referencedColumnName="id")
+     */
+    protected $categoryId;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $context;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $createdTimestamp;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $lastUpdatedTimestamp;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getCategoryId(): int
+    {
+        return $this->categoryId;
+    }
+
+    public function setCategoryId(int $categoryId): self
+    {
+        $this->categoryId = $categoryId;
+        return $this;
+    }
+
+    public function getContext(): string
+    {
+        return $this->context;
+    }
+
+    public function setContext(string $context): self
+    {
+        $this->context = $context;
+        return $this;
+    }
+
+    public function getCreatedTimestamp(): int
+    {
+        return $this->createdTimestamp;
+    }
+
+    public function setCreatedTimestamp($createdTimestamp): self
+    {
+        $this->createdTimestamp = $createdTimestamp;
+        return $this;
+    }
+
+    public function getLastUpdatedTimestamp(): int
+    {
+        return $this->lastUpdatedTimestamp;
+    }
+
+    public function setLastUpdatedTimestamp($lastUpdatedTimestamp): self
+    {
+        $this->lastUpdatedTimestamp = $lastUpdatedTimestamp;
+        return $this;
+    }
+}
