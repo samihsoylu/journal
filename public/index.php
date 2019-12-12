e4f0bd9d88e1200b789ae33397872f417ed287c2
Samih Soylu <mail@samihsoylu.nl>
2019-12-12 09:37:54 +0100

    Implemented a possible example on how notes will be save in to the database

--- public/index.php
+++ public/index.php
@@ -1,4 +1,31 @@
 <?php
+
+use App\Database\Repository\CategoryRepository;
+
 require_once(__DIR__ . '/../private/init.php');
 
-echo "File is loaded\n\n";
+$r = CategoryRepository::getByName('diary 1');
+
+print_r($r);
+//foreach ($r as $data) {
+//    /** @var Category $data */
+//    $categoryName = $data->getCategoryName();
+//    $data->setCategoryName($categoryName . ' 1');
+//    $data->save();
+//}
+
+/**
+ * Save a new note to the database (untested)
+ */
+$category = CategoryRepository::getByName('diary 1');
+$categoryId = $category->getId();
+$now = time();
+$newNote = new \App\Database\Model\Note();
+
+$newNote->setCategoryId($categoryId)
+        ->setContext('this is a test note')
+        ->setCreatedTimestamp($now)
+        ->setLastUpdatedTimestamp($now)
+        ->save()
+        ->flush();
+
