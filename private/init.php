a4b1665752ffc2c4b5cd177299f4386e35cc8121
Samih Soylu <samih@nivano.nl>
2020-04-02 22:51:35 +0200

    Now added base url to env variables

--- private/init.php
+++ private/init.php
@@ -1,11 +1,12 @@
 <?php
+// Developer debugging
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 // Project constants
-define('BASE_URL', '/');
-define('BASE_PATH', realpath(__DIR__ . '/../'));
+define('BASE_PATH', dirname(__DIR__));
 define('MODEL_PATH', BASE_PATH . '/private/lib/Database/Model/');
 define('TEMPLATE_PATH', BASE_PATH . '/private/templates/');
-define('ASSETS_URL', BASE_URL . '/assets/');
 
 // Composer dependencies
 require(BASE_PATH . '/vendor/autoload.php');
@@ -13,5 +14,8 @@ require(BASE_PATH . '/vendor/autoload.php');
 // Load environmental variables
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
-$dotenv->required(['DB_HOST', 'DB_SCHEMA', 'DB_USERNAME', 'DB_PASSWORD']);
+$dotenv->required(['DB_HOST', 'DB_SCHEMA', 'DB_USERNAME', 'DB_PASSWORD', 'BASE_URL']);
 $dotenv->required('DEV_MODE')->isBoolean();
+
+define('BASE_URL', rtrim($_ENV['BASE_URL'], '/'));
+define('ASSETS_URL', BASE_URL . '/assets');
