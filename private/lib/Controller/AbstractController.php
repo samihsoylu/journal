c9ad206a3cd41e7a5fb11c6a0730b08363466ffb
Samih Soylu <samih@nivano.nl>
2020-04-11 13:38:02 +0200

    Added template parameters to AbstractController

--- private/lib/Controller/AbstractController.php
+++ private/lib/Controller/AbstractController.php
@@ -7,9 +7,13 @@ use Jenssegers\Blade\Blade;
 abstract class AbstractController
 {
     protected Blade $template;
+    protected array $templateParams;
 
     public function __construct()
     {
         $this->template = new Blade([TEMPLATE_PATH],TEMPLATE_CACHE_PATH);
+        $this->templateParams = [
+            'assets_url' => ASSETS_URL,
+        ];
     }
 }
