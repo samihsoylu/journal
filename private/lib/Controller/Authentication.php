cabb5aeb5867799349ceaf04197412c777da6ffd
Samih Soylu <samih@nivano.nl>
2020-04-11 13:42:24 +0200

    Added url constants for the Authentication class
    The url constants will be used for routing and including the link in the required forms

--- private/lib/Controller/Authentication.php
+++ private/lib/Controller/Authentication.php
@@ -6,13 +6,19 @@ use App\Utilities\Redirect;
 
 class Authentication extends AbstractController
 {
+    public const AUTH_LOGIN_URL = BASE_URL . '/auth/login';
+    public const AUTH_LOGOUT_URL = BASE_URL . '/auth/logout';
+    public const AUTH_REGISTER_URL = BASE_URL . '/auth/register';
+
     public function register(): void
     {
         //Redirect::to(BASE_URL . '/authentication/success/');
     }
 
-    public function login(): void
+    public function login(array $vars): void
     {
+        print_r($vars);
+        echo 'Login';
     }
 
     public function logout(): void
