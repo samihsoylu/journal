32a2bcd4697a61cc231ff746cc76ce0d536e2d7a
Samih Soylu <samih@nivano.nl>
2020-04-11 13:37:24 +0200

    Improvements to environment variables

--- private/init.php
+++ private/init.php
@@ -1,26 +1,31 @@
 <?php
-// Project constants
-define('BASE_PATH', dirname(__DIR__));
-define('MODEL_PATH', BASE_PATH . '/private/lib/Database/Model/');
-define('TEMPLATE_PATH', BASE_PATH . '/private/templates/');
-define('TEMPLATE_CACHE_PATH', BASE_PATH . '/private/cache/templates/');
 
-// Composer dependencies
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// BASE_PATH = Parent directory
+define('BASE_PATH', dirname(__DIR__));
+
+// Composer autoloader
 require(BASE_PATH . '/vendor/autoload.php');
 
-// Load environmental variables
+// Load .env file
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
-$dotenv->required(['DB_HOST', 'DB_SCHEMA', 'DB_USERNAME', 'DB_PASSWORD', 'BASE_URL']);
-$dotenv->required('DEV_MODE')->isBoolean();
+$dotenv->required(['DB_HOST', 'DB_SCHEMA', 'DB_USERNAME', 'DB_PASSWORD', 'BASE_URL', 'DEBUG_MODE']);
+$dotenv->required('DEBUG_MODE')->isBoolean();
 
 define('BASE_URL', rtrim($_ENV['BASE_URL'], '/'));
-define('ASSETS_URL', BASE_URL . '/assets');
+define('DEBUG_MODE', ($_ENV['DEBUG_MODE'] === 'true'));
 
-if ($_ENV['DEBUG_MODE']) {
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-} else {
+if (!DEBUG_MODE) {
     ini_set('display_errors', 0);
     error_reporting(0);
 }
+
+// Project constants
+const MODEL_PATH          = BASE_PATH . '/private/lib/Database/Model/';
+const TEMPLATE_PATH       = BASE_PATH . '/private/templates/';
+const TEMPLATE_CACHE_PATH = BASE_PATH . '/private/cache/templates/';
+const ASSETS_URL          = BASE_URL . '/assets';
+
