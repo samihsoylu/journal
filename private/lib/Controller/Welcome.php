c1abe91594b3acb622fc070419e5d5cd644420e3
Samih Soylu <samih@nivano.nl>
2020-04-29 21:11:44 +0200

    Moved home_url to abstract and created welcome action

--- private/lib/Controller/Welcome.php
+++ private/lib/Controller/Welcome.php
@@ -6,16 +6,18 @@ use App\Utilities\Session;
 
 class Welcome extends AbstractController
 {
-    public const HOME_URL = BASE_URL . '/welcome';
-
     public function index(): void
     {
-        if (Session::get('isLoggedIn')) {
-            echo $this->getBladeInstance()->render('home');
-            exit();
-        }
+        $this->ensureUserIsNotLoggedIn();
 
-        $this->addToTemplateParameters('post_url', Auth::LOGIN_URL);
+        $this->addToBladeParameters('post_url', Authentication::LOGIN_URL);
         echo $this->getBladeInstance()->render('authenticate/login', $this->getBladeParameters());
+    }
+
+    public function welcome(): void
+    {
+        $this->ensureUserIsLoggedIn();
+
+        echo $this->getBladeInstance()->render('home');
     }
 }
