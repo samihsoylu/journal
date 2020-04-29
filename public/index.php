e85044958e51f089c794e5b4d896760d9bcd3ede
Samih Soylu <samih@nivano.nl>
2020-04-29 21:12:46 +0200

    Now using full class path for route

--- public/index.php
+++ public/index.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Router;
-
 require_once(dirname(__DIR__) . '/private/init.php');
 
-Router::route();
+\App\Router::route();
 
 // sources
 // https://primer.style/css/utilities/box-shadow#default
