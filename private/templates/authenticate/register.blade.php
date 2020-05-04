5437d42892d38b29f91207888c8c1374c8760cd3
Samih Soylu <samih@nivano.nl>
2020-05-05 00:33:28 +0200

    Created registration page

---
+++ private/templates/authenticate/register.blade.php
@@ -0,0 +1,29 @@
+@extends('base')
+
+@section('pageTitle', 'Login')
+
+@section('content')
+    <div class="col-12">
+        <div class="auth-form mt-6">
+            <h1>Register</h1>
+            <div class="Box box-shadow mt-3">
+                <div class="Box-row">
+                    @include('components/alerts')
+                    <form action="{{ $post_url }}" method="post">
+                        <label for="email">Email</label>
+                        <input class="form-control input-block" type="text" id="email" name="email" />
+
+                        <label for="username">Username</label>
+                        <input class="form-control input-block" type="text" id="username" name="username" />
+
+                        <label for="password">Password</label>
+                        <input class="form-control input-block" type="password" id="password" name="password" />
+
+                        <button class="btn btn-primary btn-block" type="submit">Register</button>
+                    </form>
+                </div>
+            </div>
+            <footer>&copy; 2020 <a href="https://github.com/samihsoylu/" target="_blank">SS</a></footer>
+        </div>
+    </div>
+@endsection
