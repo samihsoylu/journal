1575d779a6d8ed7cbf82e66183bd4c95c1de846f
Samih Soylu <mail@samihsoylu.nl>
2021-07-24 00:06:08 +0200

    Moved action buttons to a dropdown in nav-bar

--- private/templates/components/header.blade.php
+++ private/templates/components/header.blade.php
@@ -1,12 +1,12 @@
 <header>
     <!-- Account menu dropdown (desktop) -->
     <ul id="account_menu" class="dropdown-content">
-        <li><a href="{{ $account_url }}">Account</a></li>
+        <li><a href="{{ $account_url }}">Account settings</a></li>
         <li class="divider"></li>
         <li><a href="{{ $logout_url }}">Logout</a></li>
-
     </ul>
 
+    <!-- Navigation bar (mobile -->
     <ul id="mobile_menu" class="sidenav">
         <li><a href="{{ $dashboard_url }}">Dashboard</a></li>
         <li><a href="{{ $entries_url }}">Entries</a></li>
@@ -15,8 +15,26 @@
         @if ($session->userHasAdminPrivileges())
             <li><a href="{{ $users_url }}">Users</a></li>
         @endif
-        <li><a href="{{ $account_url }}">Account</a></li>
+        <li><a href="{{ $account_url }}">Account settings</a></li>
         <li><a href="{{ $logout_url }}">Logout</a></li>
+    </ul>
+
+    <!-- Entries menu dropdown (desktop) -->
+    <ul id="entries_menu" class="dropdown-content">
+        <li><a href="{{ $entries_url }}">View all entries</a></li>
+        <li><a href="{{ $create_entry_url }}">Create a entry</a></li>
+    </ul>
+
+    <!-- Categories menu dropdown (desktop) -->
+    <ul id="categories_menu" class="dropdown-content">
+        <li><a href="{{ $categories_url }}">View all categories</a></li>
+        <li><a href="{{ $create_category_url }}">Create a category</a></li>
+    </ul>
+
+    <!-- Templates menu dropdown (desktop) -->
+    <ul id="templates_menu" class="dropdown-content">
+        <li><a href="{{ $templates_url }}">View all templates</a></li>
+        <li><a href="{{ $create_template_url }}">Create a template</a></li>
     </ul>
 
     <div class="navbar-fixed">
@@ -26,9 +44,9 @@
                 <a href="#" data-target="mobile_menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                 <ul class="right hide-on-med-and-down">
                     <li @if ($active_page === 'dashboard') class="active" @endif><a href="{{ $dashboard_url }}">Dashboard</a></li>
-                    <li @if ($active_page === 'entries') class="active" @endif><a href="{{ $entries_url }}">Entries</a></li>
-                    <li @if ($active_page === 'templates') class="active" @endif><a href="{{ $templates_url }}">Templates</a></li>
-                    <li @if ($active_page === 'categories') class="active" @endif><a href="{{ $categories_url }}">Categories</a></li>
+                    <li @if ($active_page === 'entries') class="active" @endif><a class="dropdown-trigger" href="#!" data-target="entries_menu">Entries<i class="material-icons right no-margin">arrow_drop_down</i></a></li>
+                    <li @if ($active_page === 'templates') class="active" @endif><a class="dropdown-trigger" href="#!" data-target="templates_menu">Templates<i class="material-icons right no-margin">arrow_drop_down</i></a></li>
+                    <li @if ($active_page === 'categories') class="active" @endif><a class="dropdown-trigger" href="#!" data-target="categories_menu">Categories<i class="material-icons right no-margin">arrow_drop_down</i></a></li>
                     @if ($session->userHasAdminPrivileges())
                         <li @if ($active_page === 'users') class="active" @endif><a href="{{ $users_url }}">Users</a></li>
                     @endif
--- private/templates/template/all.blade.php
+++ private/templates/template/all.blade.php
@@ -5,7 +5,12 @@
 @section('content')
     @include('components/header')
     <div class="container">
-        @include('components/create-buttons')
+        <div class="row row-align button-row">
+            <div class="col s12">
+                <button class="btn btn-primary fullwidth hide-on-med-and-up mt-10" onclick="window.location.href='{{ $create_template_url }}'"><i class="material-icons">playlist_add</i> Create Template</button>
+                <button class="btn btn-primary right hide-on-small-only mr-10" onclick="window.location.href='{{ $create_template_url }}'"><i class="material-icons">playlist_add</i> Create Template</button>
+            </div>
+        </div>
         <div class="row row-align">
             <div class="col s12">
             @foreach ($templates as $template)
@@ -13,6 +18,7 @@
                     <div class="card">
                         <div class="card-content">
                             <span class="card-title">{{ $template->getTitle() }}</span>
+                            <p class="light-grey-text italic">{{ $template->getReferencedCategory()->getName() }}</p>
                         </div>
                     </div>
                 </a>
