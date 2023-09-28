f30663bdf341d9ae354d03b27e6e185d2c33ec37
Samih Soylu <mail@samihsoylu.nl>
2023-09-28 23:47:27 +0200

    Add generics docblocks to ActionHandlerInterface

--- src/Infrastructure/Port/Action/ActionHandlerInterface.php
+++ src/Infrastructure/Port/Action/ActionHandlerInterface.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace SamihSoylu\Journal\Infrastructure\Port\Action;
 
+/**
+ * @template TAction of ActionInterface
+ */
 interface ActionHandlerInterface
 {
+    /**
+     * @param TAction $action
+     */
     public function __invoke(ActionInterface $action): void;
 }
