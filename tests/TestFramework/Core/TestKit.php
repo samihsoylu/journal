0a1b469987597b0f99d40fe47d75f8829a46aa84
Samih Soylu <mail@samihsoylu.nl>
2023-12-03 23:26:13 +0100

    Create method in test framework for testing console commands

--- tests/TestFramework/Core/TestKit.php
+++ tests/TestFramework/Core/TestKit.php
@@ -6,6 +6,9 @@ namespace SamihSoylu\Journal\Tests\TestFramework\Core;
 
 use Psr\Container\ContainerInterface;
 use SamihSoylu\Journal\Tests\TestFramework\Core\TestOrm\TestOrmInterface;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Tester\CommandTester;
 
 final readonly class TestKit
 {
@@ -40,5 +43,37 @@ final readonly class TestKit
     public function getService(string $serviceId): object
     {
         return $this->container->get($serviceId);
+    }
+
+    /**
+     * @param array<array-key, mixed> $args
+     * @param array<array-key, mixed> $options
+     * @param array<string> $inputs
+     */
+    public function executeConsoleCommand(
+        Command $command,
+        array $args = [],
+        array $options = [],
+        array $inputs = []
+    ): void {
+        $options['interactive'] ??= false;
+
+        $this->assertInteractiveModeEnabledWithInputs($options, $inputs);
+
+        $application = new Application();
+        $application->add($command);
+
+        $tester = new CommandTester($application->find($command->getName()));
+        $tester->setInputs($inputs)
+            ->execute($args, $options);
+    }
+
+    private function assertInteractiveModeEnabledWithInputs(array $options, array $inputs): void
+    {
+        if ($options['interactive'] === false && $inputs !== []) {
+            throw new \RuntimeException(
+                "The 'interactive' option must be set to 'true' when providing inputs."
+            );
+        }
     }
 }
